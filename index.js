0fdc8b349d42245a6b4529eb733cf33c054f331f
Stutiagrawal10 <stutiagrawal33@gmail.com>
2023-01-24 13:01:21 +0530

    interchanged process.env.PORT and 4000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const format = require('date-format')
-const PORT = 4000 || process.env.PORT
+const PORT = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
     res.status(201).send('<h1>Hello from Stuti!</h1>')
